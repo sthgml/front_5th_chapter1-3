85b759bd313f79168c5edea5fee3da22af2a765a
박소희 <sthgml@naver.com>
2025-04-06 13:02:05 +0900

    chore: 개발환경 설정 (#2)
    * chore: 배포 환경 확인
    
    * fix: baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,25 @@
 import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default mergeConfig(
-  defineConfig({
-    plugins: [react()],
-  }),
-  defineTestConfig({
-    test: {
-      globals: true,
-      environment: "jsdom",
-      setupFiles: "./src/setupTests.ts",
-      coverage: {
-        reportsDirectory: "./.coverage",
-        reporter: ["lcov", "json", "json-summary"],
-      },
+const mode = process.env.NODE_ENV || "development";
+const env = loadEnv(mode, process.cwd(), "");
+
+const viteConfig = defineConfig({
+  plugins: [react()],
+  base: env.VITE_BASE_URL,
+});
+
+const vitestConfig = defineTestConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    coverage: {
+      reportsDirectory: "./.coverage",
+      reporter: ["lcov", "json", "json-summary"],
     },
-  }),
-);
+  },
+});
+
+export default mergeConfig(viteConfig, vitestConfig);
