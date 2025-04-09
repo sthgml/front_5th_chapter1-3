c698d0ade66b6b5c1b9b9861a8bec09edd55cd98
박소희 <sthgml@naver.com>
2025-04-10 06:46:16 +0900

    feat: 직접 구현한 hooks

--- src/@lib/hooks/useRef.ts
+++ src/@lib/hooks/useRef.ts
@@ -1,4 +1,16 @@
+import { useState } from "react";
+
+// useRef 함수의 타입 정의
+interface RefObject<T> {
+  current: T;
+}
+
+/**
+ * useState를 사용한 useRef 구현
+ * @param initialValue - 초기값
+ * @returns RefObject - { current: value } 형태의 객체
+ */
 export function useRef<T>(initialValue: T): { current: T } {
-  // React의 useState를 이용해서 만들어보세요.
-  return { current: initialValue };
+  const [ref] = useState<RefObject<T>>({ current: initialValue });
+  return ref;
 }
